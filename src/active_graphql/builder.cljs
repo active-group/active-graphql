acbae14f4a472943f3fa8cc04185d0b7866fe867
Marco Schneider <marco.schneider@active-group.de>
2017-08-02 16:35:03 +0200

    Added comments to builder.cljs.

--- src/active_graphql/builder.cljs
+++ src/active_graphql/builder.cljs
@@ -1,20 +1,40 @@
-(ns active-graphql.builder
-  (:require [active-graphql.core :as g]))
+(ns ^{:doc "Helper functions for easier construction of graphql queries. Most
+importantly, provides the functions `field`, `query` and `mutation` (refer to
+`active-graphql.examples` for usage)."}
+    active-graphql.builder
+  (:require [active-graphql.core :as g]
+            [active.clojure.condition :as c]))
 
-(defn wrap-in-graphql-arg
+(defn ^{:private true} wrap-in-graphql-arg
+  "Takes a `value` in `#{int float boolean string}` and wraps it in a graphql
+  value."
   [value]
   (cond
     (integer? value) (g/int-arg value)
     (float? value) (g/float-arg value)
     (boolean? value) (g/boolean-arg value)
-    (string? value) (g/string-arg value)))
+    (string? value) (g/string-arg value)
+    :else (c/assertion-violation `wrap-in-graphql-arg
+                                 "value of unsupported type" value)))
 
 (defn select
+  "Takes a map or seq containing pairs of keys (either keyword or string) and
+  values (in `#{int float boolean string}`) and returns a map prepared for
+  usage as a graphql map. The key is converted to a string, the val is wrapped
+  via `active-graphql.builder/wrap-in-graphql-arg`."
   [arg-pairs]
   (into {} (map (fn [[k v]]
                   [(name k) (wrap-in-graphql-arg v)]) arg-pairs)))
 
 (defn project
+  "Takes a seq (list or vector) or `args` and converts them to
+  `active-graphql.core/field*`s. Each entry is either:
+
+  * a keyword or string
+  * a vector or `[alias field-name]` (i.e. `[:id :userId]`)
+  * a `active-graphql/field`
+
+  Converts `fields` recursively."
   [args]
   (mapv (fn [key]
           (cond
